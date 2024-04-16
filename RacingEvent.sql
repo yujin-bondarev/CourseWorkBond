19bc156a28d8b74abb6f75bba04cf7f0d82b3b10
yujin-bond <124341551+yujin-bondarev@users.noreply.github.com>
2024-04-16 21:24:30 +0300

    Update RacingEvent.sql
    added more tables

--- RacingEvent.sql
+++ RacingEvent.sql
@@ -7,9 +7,13 @@ drop table if exists racing_event;
 
 drop table if exists sponsor;
 
-# drop table if exists racer;
-#
-# drop table if exists viewer;
+ drop table if exists racer;
+ 
+ drop table if exists racer_event;
+
+ drop table if exists viewer;
+ 
+ drop table if exists viewer_event;
 
 create table racing_event (
     event_id bigint not null auto_increment,
@@ -35,3 +39,57 @@ create table sponsor (
 
 insert into sponsor (sp_id, sp_name, sp_budget, date_contract, event_id) values ('1','Westmotors','100000','2024-10-14','1');
 insert into sponsor (sp_id, sp_name, sp_budget, date_contract, event_id) values ('2','Евроопт','69999.9','2024-10-10','1');
+
+create table racer (
+rc_id bigint not null auto_increment,
+rc_name varchar(30) null comment '',
+car_model varchar(30) null comment '',
+team_name varchar(30) null comment '',
+event_id bigint null comment '',
+primary key (rc_id) comment '',
+foreign key (event_id) 
+references racing_event(event_id)
+on delete no action
+on update no action);
+
+insert into racer (rc_id, rc_name, car_model, team_name, event_id) values ('1','Yujin','BMW','RBRacers','1');
+insert into racer (rc_id, rc_name, car_model, team_name, event_id) values ('2','Arthur','Audi','RoadRippers','1');
+
+create table racer_event (
+rc_id bigint null comment '',
+event_id bigint null comment '',
+foreign key (rc_id) references racer(rc_id) on delete no action on update no action,
+foreign key (event_id) references racing_event(event_id) on delete no action on update no action); 
+
+insert into racer_event (rc_id, event_id) values ('1', '1');
+insert into racer_event (rc_id, event_id) values ('1', '1');
+insert into racer_event (rc_id, event_id) values ('2', '2');
+insert into racer_event (rc_id, event_id) values ('1', '2');
+
+
+create table viewer (
+vw_id bigint not null auto_increment,
+vw_name varchar(30) null comment '',
+ticket_type varchar(30) null comment '',
+event_id bigint null comment '',
+primary key (vw_id) comment '',
+foreign key (event_id) 
+references racing_event(event_id)
+on delete no action
+on update no action);
+
+insert into viewer (vw_id, vw_name, ticket_type, event_id) values ('1','Глеб','VIP','1');
+insert into viewer (vw_id, vw_name, ticket_type, event_id) values ('2','Robert','Normal','1');
+insert into viewer (vw_id, vw_name, ticket_type, event_id) values ('3','Andrew','VIP','2');
+insert into viewer (vw_id, vw_name, ticket_type, event_id) values ('4','Zach','Normal','2');
+
+create table viewer_event (
+vw_id bigint null comment '',
+event_id bigint null comment '',
+foreign key (vw_id) references viewer(vw_id) on delete no action on update no action,
+foreign key (event_id) references racing_event(event_id) on delete no action on update no action); 
+
+insert into viewer_event (vw_id, event_id) values ('1', '1');
+insert into viewer_event (vw_id, event_id) values ('1', '2');
+insert into viewer_event (vw_id, event_id) values ('2', '1');
+insert into viewer_event (vw_id, event_id) values ('3', '1');
